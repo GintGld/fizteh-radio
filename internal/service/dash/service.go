038c94fbf4d0f0f60acd90289ee65ad1533e15a9
GintGld <gliaudialis.ga@phystech.edu>
2024-02-04 01:46:18 +0300

    cache dir delete fix

--- internal/service/dash/service.go
+++ internal/service/dash/service.go
@@ -80,7 +80,7 @@ func (d *Dash) Run(ctx context.Context) error {
 	log.Info("start dash")
 
 	// After loop stops, all generated files will be deleted
-	defer d.content.CleanUp()
+	//defer d.content.CleanUp() TODO: fix it or remove it
 	defer d.manifest.CleanUp()
 
 	if err := d.content.Init(); err != nil {
